42ea02f62c9a81b7ed2da9bd41517ff64bb06f74
Rodney Lab <codesign@rodneylab.com>
2022-02-07 07:49:59 +0000

    fix: 💫 update config for SvelteKit breaking changes

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,6 @@ const config = {
   kit: {
     adapter: adapter({}),
     // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     vite: {
       define: {
         'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
