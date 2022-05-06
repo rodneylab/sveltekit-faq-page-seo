39de145b90a0337a1a1aa4b3deda5c8cf78773fe
Rodney Lab <codesign@rodneylab.com>
2022-05-06 10:19:49 +0100

    fix: 💫 update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -5,6 +5,7 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
   kit: {
     adapter: adapter({}),
+    prerender: { default: true },
     vite: {
       define: {
         'process.env.VITE_BUILD_TIME': JSON.stringify(new Date().toISOString()),
